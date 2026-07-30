8927455332dce5d19246f218106ef1056c6defb6
diekaiju <diekaiju2772@gmail.com>
2026-07-30 20:07:30 +0530

    chore: cleanup

--- run_build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-export PATH="$HOME/gradlew-fdroid:$PATH"
-cd ~/fdroid-workspace
-# Clear status files and logs
-rm -f repo/status/*
-rm -f repo/*.log.gz
-/usr/bin/fdroid build --verbose --no-tarball --no-refresh --all
